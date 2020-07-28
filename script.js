ed2ebce9f6605cc87d1f6e9befa451137bb85f9f
Glitch (hello-webpage) <none>
2020-07-28 04:01:24 +0000

    👰🍄 Checkpoint ./index.html:276325/2855 ./script.js:276325/857

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ function getVideoPermissions(){
   }
 }
 async function main(video) {
+  const scene  = document.querySelector('a-scene')
   // Load the MediaPipe handpose model.
   const model = await handpose.load();
   // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain a
   // hand prediction from the MediaPipe graph.
   let timeLimitPassed = false
   const predictions = await model.estimateHands(video);
-  if (predictions.length > 0) {
+  renderFingers(predictions, scene)
+  // if (predictions.length > 0) {
     /*
     `predictions` is an array of objects describing each detected hand, for example:
     [
@@ -51,15 +53,30 @@ async function main(video) {
     ]
     */
 
-    for (let i = 0; i < predictions.length; i++) {
+//     for (let i = 0; i < predictions.length; i++) {
+//       const keypoints = predictions[i].landmarks;
+
+//       // Log hand keypoints.
+//       for (let i = 0; i < keypoints.length; i++) {
+//         const [x, y, z] = keypoints[i];
+//         console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+//       }
+//     }
+//  }
+}
+
+function renderFingers(predictions, scene){
+  for (let i = 0; i < predictions.length; i++) {
       const keypoints = predictions[i].landmarks;
 
       // Log hand keypoints.
       for (let i = 0; i < keypoints.length; i++) {
         const [x, y, z] = keypoints[i];
-        console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+        let spherePoint = document.createElement('a-sphere')
+        scene.appendChild(spherePoint)
+        spherePoint.setAttribute('position', {x: x/100, y:y/100, z:z})
       }
     }
-  }
 }
+
 getVideoPermissions()
