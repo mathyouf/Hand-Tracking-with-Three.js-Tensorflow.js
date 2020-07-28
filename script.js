52fc5cc4d76d6ad9e1a144f62d9c53717e7bd381
Glitch (hello-webpage) <none>
2020-07-28 03:38:25 +0000

    🎪🐿 Checkpoint ./script.js:276325/837 ./index.html:276325/467

--- script.js
+++ script.js
@@ -22,44 +22,42 @@ async function main(video) {
   const model = await handpose.load();
   // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain a
   // hand prediction from the MediaPipe graph.
-  let time = 0
-  while(time<10){
-    const predictions = await model.estimateHands(video);
-    if (predictions.length > 0) {
-      /*
-      `predictions` is an array of objects describing each detected hand, for example:
-      [
-        {
-          handInViewConfidence: 1, // The probability of a hand being present.
-          boundingBox: { // The bounding box surrounding the hand.
-            topLeft: [162.91, -17.42],
-            bottomRight: [548.56, 368.23],
-          },
-          landmarks: [ // The 3D coordinates of each hand landmark.
-            [472.52, 298.59, 0.00],
-            [412.80, 315.64, -6.18],
+  let timeLimitPassed = false
+  const predictions = await model.estimateHands(video);
+  if (predictions.length > 0) {
+    /*
+    `predictions` is an array of objects describing each detected hand, for example:
+    [
+      {
+        handInViewConfidence: 1, // The probability of a hand being present.
+        boundingBox: { // The bounding box surrounding the hand.
+          topLeft: [162.91, -17.42],
+          bottomRight: [548.56, 368.23],
+        },
+        landmarks: [ // The 3D coordinates of each hand landmark.
+          [472.52, 298.59, 0.00],
+          [412.80, 315.64, -6.18],
+          ...
+        ],
+        annotations: { // Semantic groupings of the `landmarks` coordinates.
+          thumb: [
+            [412.80, 315.64, -6.18]
+            [350.02, 298.38, -7.14],
             ...
           ],
-          annotations: { // Semantic groupings of the `landmarks` coordinates.
-            thumb: [
-              [412.80, 315.64, -6.18]
-              [350.02, 298.38, -7.14],
-              ...
-            ],
-            ...
-          }
+          ...
         }
-      ]
-      */
+      }
+    ]
+    */
 
-      for (let i = 0; i < predictions.length; i++) {
-        const keypoints = predictions[i].landmarks;
+    for (let i = 0; i < predictions.length; i++) {
+      const keypoints = predictions[i].landmarks;
 
-        // Log hand keypoints.
-        for (let i = 0; i < keypoints.length; i++) {
-          const [x, y, z] = keypoints[i];
-          console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-        }
+      // Log hand keypoints.
+      for (let i = 0; i < keypoints.length; i++) {
+        const [x, y, z] = keypoints[i];
+        console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
       }
     }
   }
