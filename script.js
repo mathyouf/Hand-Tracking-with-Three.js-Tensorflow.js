c1ec67559236abc37ca8a8f6b701f42763a6703a
Glitch (hello-webpage) <none>
2020-08-01 01:51:17 +0000

    📍🌃 Checkpoint ./script.js:276325/4763

--- script.js
+++ script.js
@@ -11,105 +11,54 @@ async function getVideoPermissions() {
   }
 }
 async function main() {
-  // Query and store the A-frame scene selector for use during render calls
-  const scene  = document.querySelector('a-scene');
+  // Make hand points
+  const points = await makeHandPoints();
   
   // Get video permissions to begin rendering what's seen in the user camera
-  const video = await getVideoPermissions()
+  const video = await getVideoPermissions();
   
   // Load the MediaPipe handpose model.
   const model = await handpose.load();
   
   let count = 0
-  let first = true
-  let points = []
-  while(count<1000 | first===true){
+  while(count<1000){
     //Obtain hand prediction from the MediaPipe graph.
     const predictions = await model.estimateHands(video);
-    console.log(predictions)
-    const render = await renderFingers(predictions, scene, first, points)
-    // const wait = await new Promise((resolve, reject)=>{setTimeout(()=>{resolve('waited')},200)})
-    if(first===true){
-      if(render.length>0){
-        first = false
-        points = render
-      }
+    if(predictions.length>0){
+      const render = await renderFingers(predictions, points)
     }
+    // const wait = await new Promise((resolve, reject)=>{setTimeout(()=>{resolve('waited')},200)})
     count++
   }
-  // const render = await renderFingers(predictions)
-  // if (predictions.length > 0) {
-    /*
-    `predictions` is an array of objects describing each detected hand, for example:
-    [
-      {
-        handInViewConfidence: 1, // The probability of a hand being present.
-        boundingBox: { // The bounding box surrounding the hand.
-          topLeft: [162.91, -17.42],
-          bottomRight: [548.56, 368.23],
-        },
-        landmarks: [ // The 3D coordinates of each hand landmark.
-          [472.52, 298.59, 0.00],
-          [412.80, 315.64, -6.18],
-          ...
-        ],
-        annotations: { // Semantic groupings of the `landmarks` coordinates.
-          thumb: [
-            [412.80, 315.64, -6.18]
-            [350.02, 298.38, -7.14],
-            ...
-          ],
-          ...
-        }
-      }
-    ]
-    */
-
-//     for (let i = 0; i < predictions.length; i++) {
-//       const keypoints = predictions[i].landmarks;
-
-//       // Log hand keypoints.
-//       for (let i = 0; i < keypoints.length; i++) {
-//         const [x, y, z] = keypoints[i];
-//         console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-//       }
-//     }
-//  }
 }
 
-async function renderFingers(predictions, scene, first, points){
-  if(first === true){
-    let pointsNodes = []
-    let handCenter = document.createElement('a-entity')
-    handCenter.classList.add('handCenter')
-    handCenter.setAttribute('position', '3 0 0')
-    // for (let i = 0; i < predictions.length; i++) {
-        const keypoints = predictions[predictions.length-1].landmarks;
-        // Log hand keypoints.
-        for (let i = 0; i < keypoints.length; i++) {
-          let [x, y, z] = keypoints[i];
-          let spherePoint = document.createElement('a-sphere')
-          spherePoint.classList.add(i+'fingerPoint')
-          spherePoint.setAttribute('radius', 0.2)
-          handCenter.appendChild(spherePoint)
-          spherePoint.setAttribute('position', {x: -x/100, y:(500-y)/100, z:z/50})
-          pointsNodes.push(spherePoint)
-        // }
-      scene.appendChild(handCenter)
-    }
-    return pointsNodes
-  } else {
-    if(predictions.length > 0){
-      for (let i = 0; i < predictions.length; i++) {
-        points.forEach((point, j) => {
-          const keypoints = predictions[i].landmarks
-          let [x, y, z] = predictions[i].landmarks[j]
-          console.log(x,y,z)
-          point.setAttribute('position', {x: -x/100, y:(500-y)/100, z:z/50})
-        })
-      }
-      return true
-    }
+async function makeHandPoints(){
+  const scene = document.querySelector('a-scene');
+  let handPointNodes = []
+  let handCenter = document.createElement('a-entity')
+  handCenter.classList.add('handCenter')
+  handCenter.setAttribute('position', '3 0 0')
+  // Create each hand point
+  for (let i = 0; i < 21; i++) {
+    let spherePoint = document.createElement('a-sphere')
+    spherePoint.classList.add(i+'fingerPoint')
+    spherePoint.setAttribute('radius', 0.15)
+    handCenter.appendChild(spherePoint)
+    handPointNodes.push(spherePoint)
   }
+  scene.appendChild(handCenter)
+  return handPointNodes
+}
+
+async function renderFingers(predictions, points){
+  // for (let i = 0; i < predictions.length; i++) {
+  points.forEach((point, j) => {
+    const keypoints = predictions[predictions.length-1].landmarks
+    let [x, y, z] = predictions[predictions.length-1].landmarks[j]
+    console.log(x,y,z)
+    point.setAttribute('position', {x: -x/100, y:(500-y)/100, z:z/50})
+  })
+  // }
+  return true
 }
 main()
