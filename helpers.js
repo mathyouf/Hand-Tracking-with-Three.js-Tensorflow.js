28d8e95ddd6592fa345e169b5201fa8ff18af458
Glitch (3dweb-hands) <none>
2020-08-02 15:03:38 +0000

    🏠💰 Checkpoint ./script.js:276325/2456 ./helpers.js:276325/1401

--- helpers.js
+++ helpers.js
@@ -11,6 +11,54 @@ async function getVideoPermissions() {
   }
 }
 
+async function makeHandPoints() {
+  const scene = document.querySelector("a-scene");
+  let handPointNodes = [];
+  let handCenter = document.createElement("a-entity");
+  handCenter.classList.add("handCenter");
+  handCenter.setAttribute("position", "3 0 0");
+  // Create each hand point
+  for (let i = 0; i < 21; i++) {
+    let spherePoint = document.createElement("a-sphere");
+    spherePoint.classList.add(i + "fingerPoint");
+    spherePoint.setAttribute("radius", 0.15);
+    let [x, y, z] = convertTo3D(startingHands[i]);
+    spherePoint.setAttribute("position", {
+      x: x,
+      y: y,
+      z: z
+    });
+    if (5 <= i && i <= 8) {
+      spherePoint.setAttribute("color", "blue");
+    }
+    if (9 <= i && i <= 12) {
+      spherePoint.setAttribute("color", "green");
+    }
+    if (13 <= i && i <= 16) {
+      spherePoint.setAttribute("color", "yellow");
+    }
+    if (17 <= i && i <= 20) {
+      spherePoint.setAttribute("color", "red");
+    }
+    if (i===4){
+      spherePoint.setAttribute("radius", 0.135)
+    }
+    if (i%4 === 0 && i>4){
+      spherePoint.setAttribute("radius", 0.1)
+    }
+    if (i%4 === 3 && i>4){
+      spherePoint.setAttribute("radius", 0.12)
+    }
+    if (i%4 === 2 && i>4){
+      spherePoint.setAttribute("radius", 0.13)
+    }
+    handCenter.appendChild(spherePoint);
+    handPointNodes.push(spherePoint);
+  }
+  scene.appendChild(handCenter);
+  return handPointNodes;
+}
+
 async function eucDist(lhs, rhs) {
   let vectors = []
   lhs.forEach((lhs_vector, i)=>{vectors.push((rhs[i] - lhs_vector))})
--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-async function makeHandPoints() {
-  const scene = document.querySelector("a-scene");
-  let handPointNodes = [];
-  let handCenter = document.createElement("a-entity");
-  handCenter.classList.add("handCenter");
-  handCenter.setAttribute("position", "3 0 0");
-  // Create each hand point
-  for (let i = 0; i < 21; i++) {
-    let spherePoint = document.createElement("a-sphere");
-    spherePoint.classList.add(i + "fingerPoint");
-    spherePoint.setAttribute("radius", 0.15);
-    let [x, y, z] = convertTo3D(startingHands[i]);
-    spherePoint.setAttribute("position", {
-      x: x,
-      y: y,
-      z: z
-    });
-    if (5 <= i && i <= 8) {
-      spherePoint.setAttribute("color", "blue");
-    }
-    if (9 <= i && i <= 12) {
-      spherePoint.setAttribute("color", "green");
-    }
-    if (13 <= i && i <= 16) {
-      spherePoint.setAttribute("color", "yellow");
-    }
-    if (17 <= i && i <= 20) {
-      spherePoint.setAttribute("color", "red");
-    }
-    if (i===4){
-      spherePoint.setAttribute("radius", 0.135)
-    }
-    if (i%4 === 0 && i>4){
-      spherePoint.setAttribute("radius", 0.1)
-    }
-    if (i%4 === 3 && i>4){
-      spherePoint.setAttribute("radius", 0.12)
-    }
-    if (i%4 === 2 && i>4){
-      spherePoint.setAttribute("radius", 0.13)
-    }
-    handCenter.appendChild(spherePoint);
-    handPointNodes.push(spherePoint);
-  }
-  scene.appendChild(handCenter);
-  return handPointNodes;
+async function updateTime(lasttime,elapsedseconds){
+  let newtime = new Date()
+  elapsedseconds += (newtime - lasttime)
+  console.log(elapsedseconds)
+  return [newtime, elapsedseconds]
 }
 
 async function main(){
@@ -55,5 +14,11 @@ async function main(){
 
   // Load the MediaPipe handpose model.
   const model = await handpose.load();
+  
+  let elapsedseconds = 0
+  let lasttime = new Date()
+  while(elapsedseconds<30){
+    [lasttime, elapsedseconds] = await updateTime(lasttime,elapsedseconds)
+  }
 }
 main()
