026ae30373fa2d56c87390a1ba5baec3f04bd04b
Glitch (3dweb-hands) <none>
2020-08-05 06:25:17 +0000

    🏚🍩 Checkpoint ./script.js:276325/285 ./index.html:276325/415

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function updateTime(lasttime,elapsedseconds){
   let newtime = new Date()
   let deltaSeconds = (newtime-lasttime)/1000
-  let fps = deltaSeconds
+  let fps = 1.0/deltaSeconds
   elapsedseconds += deltaSeconds
   return [newtime, elapsedseconds, fps]
 }
@@ -16,17 +16,16 @@ async function main(){
   // Load the MediaPipe handpose model.
   const model = await handpose.load();
   
-  let elapsedseconds = 0
+  let elapsedseconds = 0.0
   let lasttime = new Date()
   let fps = 0
   while(elapsedseconds<30){
-    const predictions = await model.estimateHands(video);
+    // const predictions = await model.estimateHands(video);
     const wait = await new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve("done");
       }, 10);
     });
-    console.log(predictions.length)
     [lasttime, elapsedseconds, fps] = await updateTime(lasttime,elapsedseconds)
     console.log(fps)
   }
